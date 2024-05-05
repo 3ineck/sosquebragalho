a74a828ed3897a29f43d1cb7e994657fa8405ed5
3ineck <60150729+3ineck@users.noreply.github.com>
2024-05-05 11:57:36 -0300

    Comentários index.js

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
+//Importação das bibliotecas express para conexão do servidor
+//e body-parser para definir a pasta padrão como a public
 import express from "express";
 import bodyParser from "body-parser";
 
+//Definição da constante app como a função express para utilização posterior
 const app = express();
 
+//Definição da pasta inicial padrão
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-/////////////////////app.get/////////////////////
+//Após esse momento é definido as routes de acesso ao site
+
+//Quando o usuário entra na página inicial
 app.get("/", function (req, res) {
   res.render("home.ejs");
 });
 
+//Quando o usuário acessa a página /fale-conosco
 app.get("/fale-conosco", function (req, res) {
   res.render("fale-conosco.ejs");
 });
 
+//Quando o usuário acesso a página /sobre-nos
 app.get("/sobre-nos", function (req, res) {
   res.render("sobre.ejs");
 });
 
-//Conexão com o servidor
+//Definição da porta do servidor
+//Código padrão para que se haja um arquivo .env com uma porta específica, funciona com essa porta
+//se não, define a porta padrão 3000.
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
@@ -27,5 +37,5 @@ if (port == null || port == "") {
 
 //Retorno da conexão
 app.listen(port, function () {
-  console.log("Server has started successfully");
+  console.log("Servidor conectado.");
 });
